f3f5ad9115d76aab48c42f66f106f6a0bc785144
adw <ehdnjs1195@naver.com>
2023-11-14 10:50:02 +0900

    10-4 short string optimization
    - SSO 짧은 문자열 최적화.
    - 문자열이 짧은 경우 메모리를 매번 할당하여 공간을 낭비할 수 없기 때문에 객체 자체에 저장해버린다.

---
+++ 231114_ch10-4/sso(short_string_optimization).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+// 이와 같이 new 를 전역 함수로 정의하면 모든 new 연산자를 오버로딩 해버린다.
+// (어떤 클래스의 멤버 함수로 정의하면 해당 클래스의 new 만 오버로딩됨)
+void* operator new(std::size_t count) {
+    std::cout << count << " bytes 할당 " << std::endl;
+    return malloc(count);
+}
+
+int main() {
+    std::cout << "s1 생성 --- " << std::endl;
+    std::string s1 = "this is a pretty long sentence!!!";
+    std::cout << "s1 크기 : " << sizeof(s1) << std::endl;
+    /*
+        s1을 생성할 때 메모리 할당이 발생.
+        s2는 메모리 할당 x. => 객체로만 생성.
+
+        하지만 s2의 크기가 32로 꽤 큰편이고, 성능 향상을 기대할 수 있다.
+    */
+
+    std::cout << "s2 생성 --- " << std::endl;
+    std::string s2 = "short sentence";
+    std::cout << "s2 크기 : " << sizeof(s2) << std::endl;
+}
