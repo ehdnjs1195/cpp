5692872e371a5549730fe8f033a15408db99512d
ADW <ehdnjs1195@naver.com>
2023-11-14 23:51:23 +0900

    백준 4949 오답임
    - 이제 정답은 출력되는 거 같은데 체점결과는 틀림.

--- 231114_baekjoon/baekjoon_4949.cpp
+++ 231114_baekjoon/baekjoon_4949.cpp
@@ -3,40 +3,94 @@
 #include <stack>
 
 using namespace std;
+void sol_by_iingang(){
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	while(true){
+		stack<char> S;
+		string str;
+		
+		getline(cin, str);
+		
+		if(str==".")
+			break;
+	
+		bool flag = true;
+			
+		for(int i=0; i<str.size(); i++){
+			
+			if(str[i] == '[' || str[i] == '('){
+				S.push(str[i]);
+			}
+			
+			else if(str[i] == ']'){
+				if(S.empty()){
+					flag = false;
+					break;
+				}
+				
+				else if(S.top() == '['){
+					S.pop();
+				}
+				
+				else if(S.top() == '('){
+					flag = false;
+					break;
+				}
+			}
+			
+			else if(str[i] == ')'){
+				if(S.empty()){
+					flag = false;
+					break;
+				}
+				
+				else if(S.top() == '('){
+					S.pop();
+				}
+				
+				else if(S.top() == '['){
+					flag = false;
+					break;
+				}
+			}
+			
+		}
+		
+		if(S.empty() && flag)
+			cout << "yes\n" ;
+			
+		else
+			cout << "no\n" ;
+	
+	}
+}
+
 int main() {
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+    
     string line;
-    stack<char> ps, check;
+    stack<char> ps;
 
     while(getline(cin,line)) {
-        if(line.empty()) break;
+        if(line.at(0) == '.') break;    // 종료 조건
+        // cout << "line: " << line << endl;
         for(auto ch : line) {
-            cout  << "ch: " << ch << endl;  //debuging
-            if(!ps.empty() && ((ps.top() == '(' && ch == ')') || (ps.top() == '[' && ch == ']'))) {
-                ps.pop();
-                check.push(ch);
-            } else if(line.size() == 1 && ch==' ') {
-                check.push(ch);
-                cout << "yes!!\n";
-                continue;
-            } else {
+            if (ch == '(' || ch == '[') {
                 ps.push(ch);
-                check.push(ch);
-            }
+            } else if(!ps.empty() && ((ps.top() == '(' && ch == ')') || (ps.top() == '[' && ch == ']'))) {
+                ps.pop();
+            } 
         }
         
-        if(ps.empty()) cout << "yes\n";
+        if (ps.empty()) cout << "yes\n";
         else cout << "no\n";
         
-        for(int i=0; i<check.size(); i++) {
-            cout << check.top();
-            check.pop();
-        cout << endl;
+        while (!ps.empty()) {   // 차있는 스택 비우기.
+            ps.pop();
         }
-
-    // for(int i=0; i<scripts.size(); i++) {
-    //     cout << scripts.top() << endl;
-    //     scripts.pop();
-    // }
     }
 
 }
