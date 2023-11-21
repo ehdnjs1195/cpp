31c7562bd872da8ec60d235d349f334c0dbcd747
ADW <ehdnjs1195@naver.com>
2023-11-21 09:50:43 +0900

    [project] web_server 구현
    - 간단하게 데이터를 받아서 출력하는 서버소켓 구현
    - 클라이언트는 구현중

---
+++ project/web_client.cpp
@@ -0,0 +1,5 @@
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
--- project/web_server.cpp
+++ project/web_server.cpp
@@ -10,14 +10,52 @@
 #define CLNT_MAX 10
 
 int g_clnt_socks[CLNT_MAX]; // 클라이언트 소켓
+int g_clnt_count = 0;
 
 int main(int argc, char ** argv){
     int serv_sock;
+    int clnt_sock;
+
+    struct sockaddr_in clnt_addr;
+    int clnt_addr_size;
+    
     struct sockaddr_in serv_addr;
     serv_sock = socket(PF_INET, SOCK_STREAM, 0);   //PF_INET: IPv4 | SOCK_STREAM: TCP 통신 | 0을 써주면 앞의 타입을 보고 자동으로 tcp면 tcp로, udp면 udp로 지정해줌.
 
-    srv_addr.sin_family = AF_INET;  // 바인드할 때 씀. IPv4를 쓰겠다는 뜻
+    serv_addr.sin_family = AF_INET;  // 바인드할 때 씀. IPv4를 쓰겠다는 뜻
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(7889);   // port번호
+
+    if(bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {  // bind(). -1 반환은 bind 실패.
+        printf("bind error\n"); 
+    }
+
+    if(listen(serv_sock, 5) == -1) {    // 5는 백로그 큐 크기. listen 대기
+        printf("listen error");
+    }
+
+    char buff[200]; // 데이터 버퍼
+    int recv_len = 0;
+    while(1) {
+        clnt_addr_size = sizeof(clnt_addr);
+        clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+
+        // g_clnt_socks[g_clnt_count++] = clnt_sock;   
+
+        while(1) {
+            recv_len = read(clnt_sock, buff, 200);
+
+            printf("recv: ");
+            for(int i=0; i<recv_len; i++){
+                printf("%02X", (unsigned char)buff[i]);
+            }
+            printf("\n");
+        }
+    }
 }
+
+
+
 /*
     socket(매개변수1, 매개변수2, 매개변수3);
     
