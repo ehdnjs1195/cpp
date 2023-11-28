89859247e98949ef378a03bd4c76e64ce4c4d538
ADW <ehdnjs1195@naver.com>
2023-11-28 10:19:43 +0900

    14 #std::function
    - function 객체에 멤버 함수를 호출하고자 하는 경우에는 어떤 객체인지를 명시해주어야 한다.
    - 따라서 인자로 객체의 주소를 전달해주어야 그 함수를 호출할 수 있다.

---
+++ 231128_ch14/function_member_func.cpp
@@ -0,0 +1,32 @@
+#include <functional>
+#include <iostream>
+#include <string>
+
+class A {
+    int c;
+
+    public:
+    A(int c) : c(c) {}
+    int some_func() {
+        std::cout << "비상수 함수: " << ++c << std::endl;
+        return c;
+    }
+
+    int some_const_function() const {
+        std::cout << "상수 함수: " << c << std::endl;
+        return c;
+    }
+
+    static void st() {}
+};
+
+int main() {
+    A a(5);
+    // std::function<int()> f1 = a.some_func;   // 함수의 입장에서 자신을 호출하는 객체가 무엇인지 모름. 따라서 어떤 객체인지를 전달해주어야 함.
+    std::function<int(A&)> f1 = &A::some_func;
+    std::function<int(const A&)> f2 = &A::some_const_function;
+
+    f1(a);  // 객체 a를 전달함으로 같은 객체 내의 멤버 함수를 호출하는 형태의 꼴이 된다.
+    f1(a);
+    f2(a);
+}
