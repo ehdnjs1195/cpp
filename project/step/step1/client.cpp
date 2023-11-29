85305f968eef10961c33022c625ce305f0f43dc0
ADW <ehdnjs1195@naver.com>
2023-11-29 13:56:33 +0900

    [project] #step1 #server2, client #일부 문제있음
    - server2 : byte data를 받아서 int data로 변환하여 16진수로 출력하였음.
    - client : 임의로 데이터를 보내는 함수를 구현하였고, 프로토콜의 seqNo가 되는 6번째 데이터를 하나씩 증가하도록 송신함.
    
    - 문제점.
    - server2의 thread join 위치가 잘못된듯.
    - client로부터 받는 가장 처음 데이터를 출력하지 못함. (01 안 나옴)

--- project/step/step1/client.cpp
+++ project/step/step1/client.cpp
@@ -4,22 +4,8 @@
 #include <sstream>
 #include <vector>
 using boost::asio::ip::udp;
-std::string protocol_data(){
-    // int data = 0x0003000c140002010102011a;
-    static long data = 0x0003000c1400;
-    // 6번째 바이트에 1을 더하기
-    int mask = 0xFF ;  // 6번째 바이트에 해당하는 비트를 마스크
-    int sixthByte = (data & mask) ;  // 현재 6번째 바이트의 값
-    sixthByte = (sixthByte + 1) % 256;  // 1을 더하고, 255를 넘으면 0으로 되돌림
-    data &= ~mask;  // 기존 6번째 바이트를 0으로 초기화
-    data |= (sixthByte );  // 새로운 6번째 바이트 설정
 
-    // data를 16진수 문자열로 변환하여 반환
-    std::stringstream ss;
-    ss << std::hex << std::setw(12) << std::setfill('0') << data;
-    return ss.str();
-}
-std::vector<char> protocol_data2(){
+std::vector<char> protocol_data(){
     static std::vector<char> data = {0x00, 0x03, 0x00, 0x0c, 0x14, 0x00};
     // std::vector<char> buffer(data.begin(), data.end());  // byte 데이터로 변형
     data[5] = (data[5] + 1) % 256; // 6번째 byte 1 더하기
@@ -33,10 +19,9 @@ int main() {
 
         udp::endpoint server_endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 8888);
         int count =0;
-        while(count++ < 5) {
-            // std::string message = protocol_data();
-            std::vector<char> message = protocol_data2();
-            socket.send_to(boost::asio::buffer(message), server_endpoint);
+        while(count++ < 256) {
+            std::vector<char> data = protocol_data();
+            socket.send_to(boost::asio::buffer(data), server_endpoint);
         }
 
     } catch(std::exception e) {
--- project/step/step1/server2.cpp
+++ project/step/step1/server2.cpp
@@ -6,29 +6,15 @@
 #include <vector>
 #include <stdio.h>
 #define PORT_NUM 8888
-#define THREAD_SLEEP_TIME_MS 100
+#define THREAD_SLEEP_TIME_MS 10
 using boost::asio::ip::udp;
 
 
-void interval(udp::socket& socket,udp::endpoint& client_endpoint , const std::chrono::milliseconds& duration) {
+void interval(udp::socket& socket,udp::endpoint& client_endpoint , const std::chrono::milliseconds& duration){
     while(true) {
         std::this_thread::sleep_for(duration);
 
-        char data[1024];
-        size_t bytes_received = socket.receive_from(boost::asio::buffer(data), client_endpoint);
-        std::string print_data = "";
-        for(int i=0; i<bytes_received; i++) {
-            if(i!=0 && i%2==0) print_data += " ";
-            print_data += data[i];
-        }
-        std::cout << "Received from [" << client_endpoint.address() << "|" << client_endpoint.port() << "]: " << print_data << std::endl;
-    }
-}
-void interval2(udp::socket& socket,udp::endpoint& client_endpoint , const std::chrono::milliseconds& duration){
-    while(true) {
-        std::this_thread::sleep_for(duration);
-
-        char data[1024];
+        unsigned char data[1024];   // char 는 최상위 비트가 1이면 부호가 바뀐다. 따라서 unsigned char 타입이어야 함.
         size_t bytes_received = socket.receive_from(boost::asio::buffer(data), client_endpoint);
         std::vector<int> receive_data(data, data+bytes_received);
 
@@ -37,7 +23,7 @@ void interval2(udp::socket& socket,udp::endpoint& client_endpoint , const std::c
             printf("%02X ", value);
         }
         std::cout << std::endl;
-
+        
     }
 }
 int main() {
@@ -52,9 +38,8 @@ int main() {
             udp::endpoint client_endpoint;
             // receive_from 함수를 사용하여 클라이언트의 연결 대기.
             size_t bytes_received = socket.receive_from(boost::asio::buffer(data), client_endpoint);
-            std::cout << bytes_received << std::endl;
             // 작업스레드: interval 형태로 소켓과 지연시간 전달
-            std::thread works = std::thread(interval2, std::ref(socket), std::ref(client_endpoint), std::chrono::milliseconds(THREAD_SLEEP_TIME_MS));
+            std::thread works = std::thread(interval, std::ref(socket), std::ref(client_endpoint), std::chrono::milliseconds(THREAD_SLEEP_TIME_MS));
             works.join();
         }
         
