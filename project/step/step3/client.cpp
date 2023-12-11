6c012ffe0f8d445b87a5e74a52f8c60f1f8344f4
ADW <ehdnjs1195@naver.com>
2023-12-11 15:17:59 +0900

    [project] #step3 #packet전송
    [구현된 부분]
    - 완벽한 패킷을 만들어서 전송하기
    - 실시간에 맞는 패킷을 만들어서 전송하는 과정은 완성한 듯.
    - seqNo는 그저 보내질 때 마다 한번씩만 증가되면 됨.
    
    [문제점&미비한점]
    1. 정확히 초가 변했을 때 send() 되는 것이 아님. 그냥 1초 sleep만 주고 있기 때문에, 정확하지 않음.
    2.  수준4(마지막)의 코드가 미완성임. 수준에 따라 패킷을 정할 수 있도록 하였지만 허접하고, 정해진 수준도 아닐 때 코드가 실행되버림.

--- project/step/step3/client.cpp
+++ project/step/step3/client.cpp
@@ -7,10 +7,15 @@
 #include <thread>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
+// 봉석님 테스트 서버 10.30.8.83 | 12001
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 8888
+
 using boost::asio::ip::udp;
 struct protocol{
     int InterID;
     std::vector<int> phase_time;
+    std::vector<int> pahse_level;
     std::vector<int> level_cycle;
     std::vector<int> level_time;
     std::vector<int> level_id;
@@ -31,10 +36,8 @@ int main() {
         std::cerr << e.what() << std::endl;
     }
 }
+
 void loop_phase(){
-    auto now = std::chrono::system_clock::now();
-    std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
-    std::tm *time = std::localtime(&currentTime);   // 현재 시간 정보를 갖고있는 객체
     // json database
     protocol prot;
     get_phase_data(prot);
@@ -45,83 +48,78 @@ void loop_phase(){
     std::vector<int> db_level_id = prot.level_id;
     int level_size = prot.level_id.size();
     int phase = 0, seqNo = 0;
+    int phase_level=0;  // 현시 저장 변수
+    bool is_phase_known_flag = false;   // 현재 현시를 아는지
+    int phase_size = db_phase_time.size() / db_level_id.size(); // phase 개수
+   
+    std::vector<int> part_phase_time;   // 현시를 파트별로 합으로 나타낸 벡터
+    int sum=0;
+    for(int i=0; i<db_phase_time.size(); i++) {
+        sum += db_phase_time[i];
+        part_phase_time.push_back(sum);
+        if((i+1)%phase_size == 0) sum = 0;
+    }
+
     while(true) {
         // 실시간
+        auto now = std::chrono::system_clock::now();
+        std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+        std::tm *time = std::localtime(&currentTime);   // 현재 시간 정보를 갖고있는 객체
         int curr_hour = time->tm_hour;
         int curr_min = time->tm_min;
         int curr_sec = time->tm_sec;
         std::cout << "hour: " << curr_hour << " | min: " << curr_min << " | sec: " << curr_sec << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));   // 1초
 
         for(int i=0; i<level_size; i++){    // level 1~3
-            if(i==level_size-1) break;  // 마지막 level이 되면
-            std::cout << "for문" << std::endl;
-            if(db_level_time[i] <= curr_hour && db_level_time[i+1] > curr_hour){          
-                std::cout << "check1" << std::endl;
+            if(i==level_size-1) break;  // 마지막 level이 되면 실행 안되도록
+            if(db_level_time[i] <= curr_hour && db_level_time[i+1] > curr_hour){    // 구간이 어딘지 확인
                 int sec = (curr_hour - db_level_time[i]) * 60 * 60 
                             + curr_min * 60
-                            + curr_sec;
-                seqNo = sec / db_level_cycle[i];        // 구간 내의 사이클, 곧 seqNo  
-                phase = (sec % db_level_cycle[i]) + 1;    // 구간 내의 현시, 곧 phase
-
-                int phase_size = db_phase_time.size() / db_level_id.size(); // phase 개수
-                
-                for(int j=0; j<phase_size; j++) {   
-                    std::cout << "check2" << " | seqNo: " << seqNo << " | phase: " << phase <<" | i: "<< i <<" | db_level_id[i]: "<< db_level_id[i] << " | (j%phase_size) + 3*(db_level_id[i]-1): " <<(j%phase_size) + 3*(db_level_id[i]-1) << " | db_phase_time[(j%phase_size) + 3*(db_level_id[i]-1)]: "<< db_phase_time[(j%phase_size) + 3*(db_level_id[i]-1)] <<" | db_phase_time: " << db_phase_time[j] <<std::endl;
-                    if(phase == db_phase_time[(j%phase_size) + 3*(db_level_id[i]-1)]){    // 각 수준별(level) 주기, 현시의 개수가 다르므로
-                        std::cout << "make_packet() 함수 실행 전. " << std::endl;
-                        std::async([&](){make_packet(prot.InterID, seqNo, j+1);}).get();      // 현시가 되었을 때 패킷정보 전달 및 비동기적함수 호출
+                            + curr_sec;                 // 구간내 시간을 sec로 변환
+                if(seqNo == 256) seqNo = 0; // seqNo는 0부터 255까지
+                phase = (sec % db_level_cycle[i])+1;    // 구간 내에서의 시간, 곧 phase
+                if(!is_phase_known_flag){       // 처음 phase 찾기
+                    for(int j=0; j<phase_size; j++){
+                        if(phase >= part_phase_time[j+(db_level_id[i]-1) * phase_size]){
+                            phase_level = j+1;
+                        }
                     }
+                    is_phase_known_flag = true;
+                }
+                std::cout << "phase: " << phase << " | part_phase_time: " << part_phase_time[(phase_level%phase_size) + 3*(db_level_id[i]-1)] << " | phase_level: " << phase_level % 3 + 1 << " | secNo: " << seqNo <<std::endl;
+                if(phase == part_phase_time[(phase_level%phase_size) + phase_size*(db_level_id[i]-1)]) {
+                    phase_level++;
+                    std::async([&](){make_packet(prot.InterID, seqNo++, (phase_level % phase_size)+ 1);}).get();        // 정해진 현시가 되었을 때 비동기적으로 패킷정보 전달
                 }
             }
         }
-        // 마지막 level
-        int sec = (curr_hour - db_level_time[level_size-1]) * 60 * 60 
-                    + curr_min * 60
-                    + curr_sec;
-        seqNo = sec / db_level_cycle[level_size-1];        // 구간 내의 사이클, 곧 seqNo  
-        phase = sec % db_level_cycle[level_size-1] + 1;    // 구간 내의 현시, 곧 phase
-
-        int phase_size = db_phase_time.size() / db_level_id.size(); // phase 개수
-        for(int i=0; i<phase_size; i++) {   
-            if(phase == db_phase_time[(i%phase_size) + db_level_id[level_size-1] - 1]){    // 각 수준별(level) 주기, 현시의 개수가 다르므로
-                std::async([&](){make_packet(prot.InterID, seqNo, i+1);}).get();      // 현시가 되었을 때 패킷정보 전달 및 비동기적함수 호출
+        if(db_level_id.back() == level_size){ // 마지막 level
+            std::cout << "last level part" << std::endl;
+            int index = level_size - 1;
+            int sec = (curr_hour - db_level_time[index]) * 60 * 60 
+                        + curr_min * 60
+                        + curr_sec;                 // 구간내 시간을 sec로 변환
+            if(seqNo == 256) seqNo = 0;
+            phase = (sec % db_level_cycle[index])+1;    // 구간 내에서의 시간, 곧 phase
+            if(!is_phase_known_flag){       // 처음 phase 찾기
+                for(int j=0; j<phase_size; j++){
+                    if(phase >= part_phase_time[j+(db_level_id[index]-1) * phase_size]){
+                        phase_level = j+1;
+                    }
+                }
+                is_phase_known_flag = true;
+            }
+            if(phase == part_phase_time[(phase_level%phase_size) + phase_size*(db_level_id[index]-1)]) {
+                std::cout << "last level part" << std::endl;
+                phase_level++;
+                std::async([&](){make_packet(prot.InterID, seqNo++, (phase_level % phase_size)+ 1);}).get();        // 정해진 현시가 되었을 때 비동기적으로 패킷정보 전달
             }
         }
-
-        // if(db_level_time[0] <= curr_hour && db_level_time[1] > curr_hour){          // 6 ~ 10   level_1
-        //     int sec = (curr_hour - db_level_time[0]) * 60 * 60 
-        //                 + curr_min * 60
-        //                 + curr_sec;
-        //     seqNo = sec / db_level_cycle[0];        // 구간 내의 사이클, 곧 seqNo  
-        //     phase = sec % db_level_cycle[0] + 1;    // 구간 내의 현시, 곧 phase
-
-        //     int phase_size = db_phase_time.size() / db_level_id.size(); // phase 개수
-        //     for(int i=0; i<phase_size; i++) {   
-        //         if(phase == db_phase_time[(i%phase_size) + db_level_id[0] - 1]){    // 각 수준별(level) 주기, 현시의 개수가 다르므로
-        //             std::async([&](){make_packet(prot.InterID, seqNo, i+1);});      // 현시가 되었을 때 패킷정보 전달 및 비동기적함수 호출
-        //         }
-        //     }
-        //     // if(phase == db_phase_time[0]){          // 각 수준별(level) 주기, 현시의 개수가 다르므로
-        //     //     std::async([](){send_packet();});
-        //     // } else if(phase == db_phase_time[1]){
-        //     //     std::async([](){send_packet();});
-        //     // } else if(phase == db_phase_time[2]){
-        //     //     std::async([](){send_packet();});
-        //     // }
-        // } else if(db_level_time[1] <= curr_hour && db_level_time[2] > curr_hour){   // 10 ~ 16  level_2
-            
-        // } else if(db_level_time[2] <= curr_hour && db_level_time[3] > curr_hour){   // 16 ~ 22  level_3
-
-        // } else {                                                                    // 22 ~ 6   level_4
-            
-        // }
-        
     }
 }
 
 void get_phase_data(protocol& prot){
-    std::cout << "get_phase_data() 실행" << std::endl;
     boost::property_tree::ptree props, plan, level, phase;
     boost::property_tree::read_json("protocol.json", props);
     // InterID 값 가져오기
@@ -142,18 +140,20 @@ void get_phase_data(protocol& prot){
             // level_id
             int id = level.second.get<int>("ID");
             prot.level_id.push_back(id);
+            
             // phase 배열 순회
             for (const auto &phase : level.second.get_child("phase")) {
                 // RingA 값 가져오기
                 int RingA = phase.second.get<int>("RingA");
                 prot.phase_time.push_back(RingA);
-                // std::cout << "RingA: " << RingA << std::endl;
+                // phase_level
+                int num = phase.second.get<int>("num");
+                prot.pahse_level.push_back(num);
             }
         }
     }
 }
 void make_packet(int interID, int seqNo, int phase){
-    std::cout << "make_packet() 실행" << std::endl;
     std::vector<char> packet;
     // 1~2 interID
     packet.push_back((interID >> 8) & 0xFF);    
@@ -183,10 +183,9 @@ void make_packet(int interID, int seqNo, int phase){
 
 void send_packet(std::vector<char> packet){
     try{
-        std::cout << "send_packet() 실행" << std::endl;
         boost::asio::io_service io_service;
         udp::socket socket(io_service, udp::v4());
-        udp::endpoint server_endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 8888);
+        udp::endpoint server_endpoint(boost::asio::ip::address::from_string(SERVER_IP), SERVER_PORT);
 
         boost::system::error_code error;
         size_t send_bytes = socket.send_to(boost::asio::buffer(packet), server_endpoint, 0, error);
@@ -194,7 +193,7 @@ void send_packet(std::vector<char> packet){
             std::cerr << "send 실패: " << error.what() << std::endl;
             return ;
         }
-        std::cout << "send to server!" << std::endl;
+        std::cout << "send to server: " << std::endl;
 
     } catch(std::exception e) {
         std::cerr << e.what() << std::endl;
