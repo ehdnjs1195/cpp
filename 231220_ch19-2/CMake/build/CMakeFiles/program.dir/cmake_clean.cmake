696ee4994239484e46f6ee5a77468a4f98c51ece
ADW <ehdnjs1195@naver.com>
2023-12-20 23:44:38 +0900

    19-2 #cmake #basic
    - cmake는 makefile을 만들기 위해 실행
    - 반드시 build 디렉토리를 하나 생성하여 cmake를 실행할 것.
    - cmake를 통해 makefile을 얻었으면 make실행하여 실행파일 획득.
    - 실행해보기

---
+++ 231220_ch19-2/CMake/build/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program.dir/main.cc.o"
+  "CMakeFiles/program.dir/main.cc.o.d"
+  "program"
+  "program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
