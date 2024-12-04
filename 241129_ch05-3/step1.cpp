81b68ab17a4ddd882e1b0ab9486080c819b48c26
ahndowon <dwahn@laonroad.com>
2024-12-05 08:17:17 +0900

    소멸자 추가.
    소멸자는 동적 생성된 데이터를 거꾸로 올라가면서 아래서 부터 삭제해주어야 한다
    size 또한 동적으로 할당된 메모리이므로 잊지않고 해제해주어야 한다.

--- 241129_ch05-3/step1.cpp
+++ 241129_ch05-3/step1.cpp
@@ -23,17 +23,50 @@ public:
     // address 를 초기화 하는 함수이다. 재귀 호출로 구성되어 있다.
     void initialize_address(Address *current) {
     if (!current) return;
-        if (current->level == dim - 1)  // 두 번째 질문 (종료 조건)
+        if (current->level == dim - 1)  // 두 번째 질문 (종료 조건) => 즉, 마지막 차원(최하위 차원)은 실제 값이 들어가는 int배열
         {
             current->next = new int[size[current->level]];
             return;
         }
-        current->next = new Address[size[current->level]];
+        current->next = new Address[size[current->level]]; // 그 중간 차원은 Address 구조체의 배열을 할당한다
         for (int i = 0; i != size[current->level]; i++)
         {   // 다음 단계로 넘어가는 과정
             (static_cast<Address *>(current->next) + i)->level = current->level + 1;
 
             initialize_address(static_cast<Address *>(current->next) + i);
         }
+    }
+
+    void delete_address(Address *current)
+    {
+        if (!current) return;
+        for (int i = 0; current->level < dim - 1 && i < size[current->level]; i++) {
+            delete_address(static_cast<Address *>(current->next) + i);
+        }
+
+        if (current->level == dim - 1) {    // 최하위 차원일 때는
+            delete[] static_cast<int *>(current->next); // int배열을 해제
+        }
+
+        delete[] static_cast<Address *>(current->next); // 중간 차원에서 Address배열을 해제함
+    }
+
+    void copy_address(Address *dst, Address *src) {
+        if (dst->level == dim - 1) {
+            for (int i = 0; i < size[dst->level]; ++i)
+            static_cast<int *>(dst->next)[i] = static_cast<int *>(src->next)[i];
+            return;
+        }
+        for (int i = 0; i != size[dst->level]; i++) {
+            Address *new_dst = static_cast<Address *>(dst->next) + i;
+            Address *new_src = static_cast<Address *>(src->next) + i;
+            copy_address(new_dst, new_src);
+        }
+    }
+
+
+    ~Array() {
+        delete_address(top);
+        delete[] size;
     }
 };
